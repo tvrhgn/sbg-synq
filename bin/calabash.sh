3a3ce91f57fb20b500d0cb3ff65dbee15b610708
Thijs Verhagen <thijs.vrhgn@gmail.com>
2012-04-01 22:03:02 +0200

    vernieuwing ns fase I

--- bin/calabash.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# example xproc commandline
-SBG_SYNQ_HOME=..
-
-java  -cp $SBG_SYNQ_HOME/lib/calabash.jar:$SBG_SYNQ_HOME/lib/saxon9he.jar:$SBG_SYNQ_HOME/commons-codec-1.3.jar:$SBG_SYNQ_HOME/lib/commons-httpclient-3.1.jar:$SBG_SYNQ_HOME/lib/commons-logging-1.1.1.jar com.xmlcalabash.drivers.Main $@
-
-echo Done.
